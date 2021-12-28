2fe7e2fd08de9d91c55b5d0f317244c0ee663f6b
Harold <andresdias24@gmail.com>
2021-12-27 19:21:41 -0500

    configurando el inicio del proyecto y iniciando js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const listaLi = document.querySelectorAll('#numeros li');
+const listaLi =  [1,3,4,443,34] ;
 const mensaje = `hay ${listaLi.length} elementos en la lista UL "NUMEROS"`;
 console.log(mensaje);
-document.getElementById('output').innerHTML = mensaje;
+document.getElementById('products').innerHTML = mensaje;
