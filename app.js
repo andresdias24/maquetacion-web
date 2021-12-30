188555f3778ec957f87e1473b2fd12899c059207
Harold <andresdias24@gmail.com>
2021-12-29 23:08:21 -0500

    aplicando fetch a elapi de la nasa

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const listaLi =  [1,3,4,443,34] ;
 const mensaje = `hay ${listaLi.length} elementos en la lista UL "NUMEROS"`;
 console.log(mensaje);
-document.getElementById('products').innerHTML = mensaje;
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2021-06-19", requestOptions)
+    .then(response => response.text())
+    .then(result => mostrarImagen(result))
+    .catch(error => console.log('error', error));
+
+    const mostrarImagen =  (data) => {
+        var myobj = JSON.parse(data);
+        console.log(myobj.hdurl)
+
+        body = `<img src="${myobj.hdurl}">`
+        document.getElementById('products').innerHTML = body;
+
+    }
